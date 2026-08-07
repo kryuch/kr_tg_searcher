a1f40f2711faadf0e6e13fbc0e7253c14c9604c7
o.kryuchin <o.kryuchin@rtk-healthlab.ru>
2026-08-07 10:30:19 +0300

    Добавление недостающий файлов

---
+++ src/main/java/ru/kryuch/krtg/searcher/config/LiquibaseConfig.java
@@ -0,0 +1,26 @@
+package ru.kryuch.krtg.searcher.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Bean
+    public ApplicationRunner liquibaseRunner(DataSource dataSource) {
+        return new ApplicationRunner() {
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                SpringLiquibase liquibase = new SpringLiquibase();
+                liquibase.setDataSource(dataSource);
+                liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.xml");
+                liquibase.afterPropertiesSet();  // Запускаем миграции
+            }
+        };
+    }
+}
